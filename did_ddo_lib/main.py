aef7a12a48525c0d469edc9d21c4c0dae3900444
Bill Barman <bill@anantya.com>
2018-10-02 09:51:16 +0800

    add uri parsing to allow for ABNF ruling, remove regexp escape

--- did_ddo_lib/main.py
+++ did_ddo_lib/main.py
@@ -9,12 +9,16 @@ from web3 import (
     Web3
 )
 
+from urllib.parse import (
+    urlparse,
+)
+
 
 # generate a DID based in it's id, path, fragment and method
 def did_generate(id, path = None, fragment = None, method = 'ocean'):
 
     method = re.sub('[^a-z0-9]', '', method.lower())
-    id = re.sub('[^a-zA-Z0-9\-\.]', '', id)
+    id = re.sub('[^a-zA-Z0-9-.]', '', id)
     did = ['did:', method, ':', id]
     if path:
         did.append('/')
@@ -27,7 +31,7 @@ def did_generate(id, path = None, fragment = None, method = 'ocean'):
 # split a DID into it's parts
 def did_split(did):
     result = None
-    match = re.match('^did:([a-z0-9]+):([a-zA-Z0-9\-\.]+)(.*)', did)
+    match = re.match('^did:([a-z0-9]+):([a-zA-Z0-9-.]+)(.*)', did)
     if match:
         result = {
             'method' : match.group(1),
@@ -35,15 +39,10 @@ def did_split(did):
             'path': None,
             'fragment': None
         }
-        tail_text = match.group(3)
-        if tail_text and len(tail_text) > 0:
-            match = re.match('.*\#(.*)$', tail_text)
-            if match:
-
-                result['fragment'] = match.group(1)
-                # now remove ending fragment '#...'
-                tail_text = re.sub('\#.*$', '', tail_text)
-
-            if len(tail_text) > 0 and tail_text[0] == '/':
-                result['path'] = tail_text[1:]
+        uri_text = match.group(3)
+        if uri_text and len(uri_text) > 0:
+            uri = urlparse(uri_text)
+            result['fragment'] = uri.fragment
+            if len(uri.path) > 0:
+                result['path'] = uri.path[1:]
     return result
