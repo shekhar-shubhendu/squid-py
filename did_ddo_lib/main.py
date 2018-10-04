63d3d4e745e2b33176015fdf5bab75e7d7defa24
Bill Barman <bill@anantya.com>
2018-10-04 16:37:26 +0800

    cleanup did generation and validation

--- did_ddo_lib/main.py
+++ did_ddo_lib/main.py
@@ -28,25 +28,31 @@ def did_generate(id, path = None, fragment = None, method = 'ocean'):
         did.append(fragment)
     return "".join(did)
 
+# generate a base did-id, using user defined id, and ddo
 def did_generate_base_id(id, ddo):
     values = []
     values.append(id)
-    values.append(Web3.toHex(ddo.calculate_hash()))
-    return Web3.toHex(Web3.sha3(text="".join(values)[2:]))[2:]
+    # remove the leading '0x' on the DDO hash
+    values.append(Web3.toHex(ddo.calculate_hash())[2:])
+    # return the hash as a string with no leading '0x'
+    return Web3.toHex(Web3.sha3(text="".join(values)))[2:]
     
+# generate a new DID from a configured DDO, returns the new DID, and a new DDO with the id values already assigned
 def did_generate_from_ddo(id, ddo, path = None, fragment = None, method = 'ocean'):
     base_id = did_generate_base_id(id, ddo)
     did =  did_generate(base_id, method = method)
     assigned_ddo = ddo.create_new(did)
     return did_generate(base_id, path, fragment, method), assigned_ddo
     
+# validate a DID and check to see it matches the user defined 'id', and DDO
 def did_validate(did, id, ddo):
-    
     base_id = did_generate_base_id(id, ddo)
     did_items = did_parse(did)
-    return did_items['id'] == base_id
+    if did_items:
+        return did_items['id'] == base_id
+    return False
     
-# split a DID into it's parts
+# parse a DID into it's parts
 def did_parse(did):
     result = None
     match = re.match('^did:([a-z0-9]+):([a-zA-Z0-9-.]+)(.*)', did)
