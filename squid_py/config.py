bec3d7e127b60a4833e1bab0cd7232f871351ff3
billbsing <billbsing.gmail.com>
2018-10-08 12:42:24 +0800

    new load config

---
+++ squid_py/config.py
@@ -0,0 +1,112 @@
+"""Config data
+
+"""
+
+
+import os
+import configparser
+import logging
+import site
+
+from squid_py.constants import (
+    KEEPER_CONTRACTS
+)
+
+DEFAULT_KEEPER_HOST = 'localhost'
+DEFAULT_KEEPER_PORT = 8545
+DEFAULT_KEEPER_URL = 'http://localhost:8545'
+DEFAULT_KEEPER_PATH = 'contracts'
+DEFAULT_GAS_LIMIT = 300000
+DEFAULT_NAME_PROVIDER_URL = 'http://localhost:5000'
+
+NAME_KEEPER_URL = 'keeper.url'
+NAME_KEEPER_PATH = 'keeper.path'
+NAME_GAS_LIMIT = 'gas_limit'
+NAME_PROVIDER_URL = 'provider_url'
+NAME_MARKET_ADDRESS = 'market.address'
+NAME_AUTH_ADDRESS = 'auth.address'
+NAME_TOKEN_ADDRESS = 'token.address'
+
+environ_names = {
+    NAME_KEEPER_URL : ['KEEPER_URL', 'Keeper URL'],
+    NAME_KEEPER_PATH : ['KEEPER_PATH', 'Path to the keeper contracts'],
+    NAME_GAS_LIMIT : ['GAS_LIMIT', 'Gas limit'],
+    NAME_PROVIDER_URL : [ 'PROVIDER_URL', 'Provider URL'],
+    NAME_MARKET_ADDRESS  : ['MARKET_ADDRESS', 'Market address'],
+    NAME_AUTH_ADDRESS  : ['AUTH_ADDRESS', 'Auth address'],
+    NAME_TOKEN_ADDRESS : ['TOKEN_ADDRESS', 'Token address'],
+}
+
+config_defaults = {
+    KEEPER_CONTRACTS : {
+        NAME_KEEPER_URL : DEFAULT_KEEPER_URL,
+        NAME_KEEPER_PATH : DEFAULT_KEEPER_PATH,
+        NAME_GAS_LIMIT : DEFAULT_GAS_LIMIT,
+        NAME_PROVIDER_URL : DEFAULT_NAME_PROVIDER_URL,
+        NAME_MARKET_ADDRESS : '',
+        NAME_AUTH_ADDRESS : '',
+        NAME_TOKEN_ADDRESS: '',
+    }
+}
+
+class Config(configparser.ConfigParser):
+
+    def __init__(self, filename = None, **kwargs):
+        configparser.ConfigParser.__init__(self)
+
+        self.read_dict(config_defaults)
+        self._section_name = KEEPER_CONTRACTS
+        self._logger = kwargs.get('logger', logging.getLogger(__name__))
+        self._logger.debug('Config: loading config file %s', filename)
+        if filename:
+            with open(filename) as fp:
+                lines = fp.read()
+    #            lines = ("[{}]\n".format(configparser.DEFAULTSECT)) + lines
+                self.read_string(lines)
+        self._load_environ()
+
+    def _load_environ(self):
+        for option_name, environ_item in environ_names.items():
+            value = os.environ.get(environ_item[0])
+            if value != None:
+                self._logger.debug('Config: setting environ %s = %s', option_name, value)
+                self.set(self._section_name, option_name , value)
+
+    def set_arguments(self, items):
+        for name, value in items.items():
+            if value != None:
+                self._logger.debug('Config: setting argument %s = %s', name, value)
+                self.set(self._section_name, name , value)
+
+    def get_keeper_path(self):
+        path = self.get(self._section_name, NAME_KEEPER_PATH)
+        if os.path.exists(path):
+            pass
+        elif os.getenv('VIRTUAL_ENV') is not None:
+            path = os.path.join(os.getenv('VIRTUAL_ENV'), 'contracts')
+        else:
+            path =  os.path.join(site.PREFIXES[0], 'contracts')
+        return path
+
+    def get_keeper_url(self):
+        return self.get(self._section_name, NAME_KEEPER_URL)
+
+    def get_gas_limit(self):
+        return self.get(self._section_name, NAME_GAS_LIMIT)
+
+    def get_provider_url(self):
+        return self.get(self._section_name, NAME_PROVIDER_URL)
+
+    def get_address_list(self):
+        return {
+            'market': self.get(self._section_name, NAME_MARKET_ADDRESS),
+            'auth' :  self.get(self._section_name, NAME_AUTH_ADDRESS),
+            'token' :  self.get(self._section_name, NAME_TOKEN_ADDRESS),
+        }
+
+    @staticmethod
+    def get_environ_help():
+        result = []
+        for option_name, environ_item in environ_names.items():
+            result.append("{:20}{:40}".format(environ_item[0], environ_item[1]))
+        return "\n".join(result)
--- squid_py/ocean.py
+++ squid_py/ocean.py
@@ -12,11 +12,81 @@ from squid_py.log import setup_logging
 from squid_py.metadata import Metadata
 from squid_py.utils.web3_helper import Web3Helper
 
+from squid_py.config import (
+    Config,
+)
+
+CONFIG_FILE_ENVIRONMENT_NAME = 'CONFIG_FILE'
+
 setup_logging()
 
 
 class Ocean(object):
-    def __init__(self, host=None, port=None, config_path=None):
+
+    """Create a new Ocean object for access to the Ocean Protocol Network
+
+    :param keeper_url: URL of the Keeper network to connect too.
+    :param address_list: Dictinorary of contract addresses.
+        [
+            'market': '0x00..00',
+            'auth' : '0x00..00',
+            'token' : '0x00..00',
+        ]
+    :param web3: Web3 object to use as the Ethereum node.
+    :param keeper_path: Path to the Ocean Protocol Keeper contracts, to load contracts and addresses via the artifacts folder.
+    :param logger: Optional logger to use instead of creating our own loggger
+    :param provider_url: url of the provider
+    :param gas_limit: Optional gas limit, defaults to 300000
+    :param config_file: Optional config file to load in the above config
+    :returns: Ocean object
+
+    An example in creating an Ocean object::
+
+        address = [
+            'market': '0x00..00',
+            'auth' : '0x00..00',
+            'token' : '0x00..00',
+        ]
+        ocean = Ocean(url='http://localhost:8545', provider_url = 'http://localhost:5000', address_list = address)
+        print(ocean.accounts[0])
+
+    """
+
+    def __init__(self, **kwargs):
+        self._w3 = None
+        self._logger = kwargs.get('logger') or logging.getLogger(__name__)
+
+        config_file = kwargs.get('config_file', os.getenv(CONFIG_FILE_ENVIRONMENT_NAME) or None)
+
+        config = Config(config_file)
+
+        self._keeper_url = kwargs.get('keeper_url', config.get_keeper_url())
+        self._keeper_path = kwargs.get('keeper_path', config.get_keeper_path())
+        self._gas_limit = kwargs.get('gas_limit', config.get_gas_limit())
+        self._provider_url = kwargs.get('provider_url', config.get_provider_url())
+
+        # put a priority on getting the contracts directly instead of via the 'ocean node'
+
+        # load in the contract addresses
+        self._address_list = config.get_address_list()
+        if 'address_list' in kwargs:
+            address_list = kwargs['address_list']
+            for name, value in self._address_list.items():
+                if name in address_list and address_list[name]:
+                    self._address_list[name] = address_list[name]
+
+
+        if  self._keeper_url == None:
+            raise TypeError('You must provide a Keeper url')
+
+        if 'web3' in kwargs:
+            self._web3 = kwargs['web3']
+        else:
+            if self._keeper_url:
+                self._web3 = Web3(HTTPProvider(self._keeper_url))
+        if self._web3 == None:
+            raise ValueError('You need to provide a valid Keeper host and port connection values')
+        """
         try:
             config_path = os.getenv('CONFIG_FILE') if not config_path else config_path
             self.config = load_config_section(config_path, KEEPER_CONTRACTS)
@@ -28,15 +98,21 @@ class Ocean(object):
             logging.error('OceanContracts could not initiate. You can specify the path in $CONFIG_FILE environment '
                           'variable.')
             raise Exception('You should provide a valid config file.')
-        self.node_uri = "%s:%s" % (self.host, self.port)
+
+
         self.default_gas = get_value(self.config, 'gas.limit', 'GAS_LIMIT', 300000)
-        self.helper = Web3Helper(self.web3, self.config)
         self.provider_uri = get_value(self.config, 'provider.uri', 'PROVIDER_URI', 'http://localhost:5000')
-        self.metadata = Metadata(self.provider_uri)
+        """
+#        self.node_uri = "%s:%s" % (self.host, self.port)
+        self.helper = Web3Helper(self._web3, self._keeper_path, self._address_list)
+        self.metadata = Metadata(self._provider_url)
         self.market = Market(self.helper)
         self.auth = Auth(self.helper)
         self.token = Token(self.helper)
-        self.network = self.helper.get_network_name()
+        self._network_name = self.helper.get_network_name()
+
+    def get_web3(self):
+        return self._web3
 
     @staticmethod
     def connect_web3(host, port='8545'):
