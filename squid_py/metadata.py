fbdb477c2c4b000c8c3cbad0ef9834360f9024ff
eruizgar91 <enrique@oceanprotocol.com>
2018-10-10 15:12:23 +0200

    Add search method in metadata

--- squid_py/metadata.py
+++ squid_py/metadata.py
@@ -1,3 +1,4 @@
+import ast
 import json
 
 import requests
@@ -9,20 +10,25 @@ class Metadata(object):
         self.base_url = provider_uri + '/api/v1/provider/assets'  # http://localhost:5000/api/v1/provider/assets/
         self.headers = {'content-type': 'application/json'}
 
-    def get_assets(self):
+    def list_assets(self):
         return json.loads(requests.get(self.base_url).content)
 
-    def get_asset_ddo(self, asset_did):
+    def get_asset_metadata(self, asset_did):
         return json.loads(requests.get(self.base_url + '/metadata/%s' % asset_did).content)
 
-    def get_assets_metadata(self):
+    def list_assets_metadata(self):
         return json.loads(requests.get(self.base_url + '/metadata').content)
 
-    def register_asset(self, data):
+    def publish_asset_metadata(self, data):
         return json.loads(requests.post(self.base_url + '/metadata', data=json.dumps(data), headers=self.headers).content)
 
-    def update_asset(self, data):
+    def update_asset_metadata(self, data):
         return json.loads(requests.put(self.base_url + '/metadata', data=json.dumps(data), headers=self.headers).content)
 
-    def retire_asset(self, asset_id):
+    def search(self, search_query):
+        return ast.literal_eval(json.loads(
+            requests.post(self.base_url + '/metadata/query', data=json.dumps(search_query),
+                          headers=self.headers).content))
+
+    def retire_asset_metadata(self, asset_id):
         return requests.delete(self.base_url + '/metadata/%s' % asset_id, headers=self.headers)
