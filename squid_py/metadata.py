21ac9b7c497fa7a1366b9e499dc398061bc742f3
eruizgar91 <enrique@oceanprotocol.com>
2018-10-12 09:13:03 +0200

    Fix failing test

--- squid_py/metadata.py
+++ squid_py/metadata.py
@@ -11,24 +11,26 @@ class Metadata(object):
         self._headers = {'content-type': 'application/json'}
 
     def list_assets(self):
-        return json.loads(requests.get(self.base_url).content)
+        return json.loads(requests.get(self._base_url).content)
 
     def get_asset_metadata(self, asset_did):
-        return json.loads(requests.get(self.base_url + '/metadata/%s' % asset_did).content)
+        return json.loads(requests.get(self._base_url + '/metadata/%s' % asset_did).content)
 
     def list_assets_metadata(self):
-        return json.loads(requests.get(self.base_url + '/metadata').content)
+        return json.loads(requests.get(self._base_url + '/metadata').content)
 
     def publish_asset_metadata(self, data):
-        return json.loads(requests.post(self.base_url + '/metadata', data=json.dumps(data), headers=self.headers).content)
+        return json.loads(
+            requests.post(self._base_url + '/metadata', data=json.dumps(data), headers=self._headers).content)
 
     def update_asset_metadata(self, data):
-        return json.loads(requests.put(self.base_url + '/metadata', data=json.dumps(data), headers=self.headers).content)
+        return json.loads(
+            requests.put(self._base_url + '/metadata', data=json.dumps(data), headers=self._headers).content)
 
     def search(self, search_query):
         return ast.literal_eval(json.loads(
-            requests.post(self.base_url + '/metadata/query', data=json.dumps(search_query),
-                          headers=self.headers).content))
+            requests.post(self._base_url + '/metadata/query', data=json.dumps(search_query),
+                          headers=self._headers).content))
 
     def retire_asset_metadata(self, asset_id):
-        return requests.delete(self.base_url + '/metadata/%s' % asset_id, headers=self.headers)
+        return requests.delete(self._base_url + '/metadata/%s' % asset_id, headers=self._headers)
