b3351ba079dbfbe87cc46cf02ef1156020b08609
MarcusJones <jones.0bj3@gmail.com>
2018-10-24 12:50:16 +0200

    add DDO as a dict-like object

--- squid_py/asset.py
+++ squid_py/asset.py
@@ -1,4 +1,6 @@
-
+import os
+import logging
+import json
 
 class Asset:
     def __init__(self, asset_id, publisher_id, price):
@@ -74,3 +76,54 @@ class Asset:
 
     def __str__(self):
         return "Asset {} for {}, published by {}".format(self.asset_id, self.price,self.publisher_id)
+
+
+class DDO(dict):
+    def __init__(self):
+        """
+        DDO is a dictionary-like class (subclasses python dict)
+
+        The default __init__ constructor should never be called directly, but instead
+        called by the various constructor methods.
+        """
+        super().__init__()
+
+    @classmethod
+    def from_json_file(cls,json_file_path):
+        """
+        Load a DDO file as a dictionary-like object
+        :param json_file_path: path to a DDO json file
+        :return: DDO object
+        """
+        assert os.path.exists(json_file_path), "{} not found".format(json_file_path)
+
+        # Load the file into dict
+        with open(json_file_path) as f:
+            json_dict = json.load(f)
+
+        # Instantiate the dictionary
+        this_ddo = cls()
+        # Append all loaded values from the dictionary
+        this_ddo.update(json_dict)
+        return this_ddo
+
+    @classmethod
+    def from_json_string(cls,json_string):
+        """
+        Load a DDO string as a dictionary-like object
+        :param json_string: json string
+        :return: DDO object
+        """
+        pass
+
+    @property
+    def valid(self):
+        required_keys = ['@context', 'id', 'publicKey', 'authentication', 'service']
+        return all(req_key in self for req_key in required_keys)
+
+#%%
+path = r'/home/batman/ocn/squid-py/tests/resources/ddo/sample1.json'
+ddo1 = DDO.from_json_file(path)
+assert ddo1.valid
+ddo1.keys()
+ddo1['id']
