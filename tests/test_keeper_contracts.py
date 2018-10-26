9019a0be49993e0c8cee44a79ec0852e387f86fc
Bill Barman <bill@anantya.com>
2018-10-26 17:27:13 +0800

    remove trailing spaces

--- tests/test_keeper_contracts.py
+++ tests/test_keeper_contracts.py
@@ -1,6 +1,6 @@
 """
-    Test Keeper Contracts 
-    
+    Test Keeper Contracts
+
     This tests basic contract loading and one call to the smart contract to prove
     that the contact can be loaded and used
 
@@ -16,40 +16,40 @@ def get_ocean_instance():
     os.environ['CONFIG_FILE'] = path_config
     ocean = Ocean(os.environ['CONFIG_FILE'])
     return ocean
-    
+
 def test_auth_contract():
-    
+
     ocean = get_ocean_instance()
     assert ocean
-    
+
     assert ocean.keeper.auth
-    
+
     test_id = secrets.token_hex(32)
     assert ocean.keeper.auth.get_order_status(test_id) == 0
-    
+
 
 def test_didresitry_contract():
-    
+
     ocean = get_ocean_instance()
     assert ocean
-    
+
     assert ocean.keeper.didregistry
     test_id = secrets.token_hex(32)
     # contract call does not work with docker
     assert ocean.keeper.didregistry.get_update_at(test_id) == 0
 
 def test_market_contract():
-        
+
     ocean = get_ocean_instance()
     assert ocean
-    
+
     assert ocean.keeper.market
     test_id = secrets.token_hex(32)
     assert ocean.keeper.market.verify_order_payment(test_id)
 
 
 def test_token_contract():
-        
+
     ocean = get_ocean_instance()
     assert ocean
 
