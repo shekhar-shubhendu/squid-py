ccbcd17ddd4c34b01a84f0f7dd4e22c74e89957d
Lev Berman <ldmberman@gmail.com>
2018-10-30 11:58:08 +0100

    Add empty interface for registering service
    The identifier needs to be stored locally; a generic event listener is to be implemented

---
+++ squid_py/service_agreement/event_listener.py
@@ -0,0 +1,7 @@
+def watch_service_events(service_id, service_definition, actor_type, num_confirmations=12):
+    """ Subscribes to the events defined in the given service definition, targeted
+        for the given actor type. Filters events by the given service ID.
+
+        The service definition format is described in OEP-11.
+    """
+    pass
