059b094012eec6e40e6309bc3d8628bdf916b348
eruizgar91 <enrique@oceanprotocol.com>
2018-10-30 15:35:15 +0100

    Fix utilities

--- squid_py/utils/utilities.py
+++ squid_py/utils/utilities.py
@@ -6,98 +6,90 @@ from threading import Thread
 Signature = namedtuple('Signature', ('v', 'r', 's'))
 
 
-class Utilities(object):
-    def __init__(self, web3):
-        self._web3 = web3
+def sign(web3, account_address, message):
+    return web3.eth.sign(account_address, message)
 
-    def sign(self, account_address, message):
-        return self._web3.eth.sign(account_address, message)
 
-    def get_balance(self, account_address, block_identifier):
-        return self._web3.eth.getBalance(account_address, block_identifier)
+def get_balance(web3, account_address, block_identifier):
+    return web3.eth.getBalance(account_address, block_identifier)
 
-    def watch_event(self, contract_name, event_name, callback, interval, fromBlock=0, toBlock='latest', filters=None, ):
-        event_filter = self.install_filter(
-            contract_name, event_name, fromBlock, toBlock, filters
-        )
-        event_filter.poll_interval = interval
-        Thread(
-            target=self.watcher,
-            args=(event_filter, callback),
-            daemon=True,
-        ).start()
-        return event_filter
 
-    def install_filter(self, contract, event_name, fromBlock=0, toBlock='latest', filters=None):
-        # contract_instance = self.contracts[contract_name][1]
-        event = getattr(contract.events, event_name)
-        event_filter = event.createFilter(
-            fromBlock=fromBlock, toBlock=toBlock, argument_filters=filters
-        )
-        return event_filter
+def watch_event(contract_name, event_name, callback, interval, fromBlock=0, toBlock='latest', filters=None, ):
+    event_filter = install_filter(
+        contract_name, event_name, fromBlock, toBlock, filters
+    )
+    event_filter.poll_interval = interval
+    Thread(
+        target=watcher,
+        args=(event_filter, callback),
+        daemon=True,
+    ).start()
+    return event_filter
 
-    def to_32byte_hex(self, val):
-        return self._web3.toBytes(val).rjust(32, b'\0')
 
-    def split_signature(self, signature):
-        v = self._web3.toInt(signature[-1])
-        r = self.to_32byte_hex(int.from_bytes(signature[:32], 'big'))
-        s = self.to_32byte_hex(int.from_bytes(signature[32:64], 'big'))
-        if v != 27 and v != 28:
-            v = 27 + v % 2
-        return Signature(v, r, s)
+def install_filter(contract, event_name, fromBlock=0, toBlock='latest', filters=None):
+    # contract_instance = self.contracts[contract_name][1]
+    event = getattr(contract.events, event_name)
+    event_filter = event.createFilter(
+        fromBlock=fromBlock, toBlock=toBlock, argument_filters=filters
+    )
+    return event_filter
 
-    # properties
 
-    @property
-    def accounts(self):
-        """Return the accounts in the current network."""
+def to_32byte_hex(web3, val):
+    return web3.toBytes(val).rjust(32, b'\0')
+
+
+def split_signature(web3, signature):
+    v = web3.toInt(signature[-1])
+    r = to_32byte_hex(web3, int.from_bytes(signature[:32], 'big'))
+    s = to_32byte_hex(web3, int.from_bytes(signature[32:64], 'big'))
+    if v != 27 and v != 28:
+        v = 27 + v % 2
+    return Signature(v, r, s)
+
+
+# properties
+
+@property
+def network_name(web3):
+    """Give the network name."""
+    network_id = web3.version.network
+    switcher = {
+        1: 'Main',
+        2: 'orden',
+        3: 'Ropsten',
+        4: 'Rinkeby',
+        42: 'Kovan',
+    }
+    return switcher.get(network_id, 'development')
+
+
+# static methods
+def watcher(event_filter, callback):
+    while True:
         try:
-            return self._web3.eth.accounts
-        except Exception as e:
-            logging.error("Error obtaining accounts")
-            raise Exception(e)
+            events = event_filter.get_all_entries()
+        except ValueError as err:
+            # ignore error, but log it
+            print('Got error grabbing keeper events: ', str(err))
+            events = []
 
-    @property
-    def web3(self):
-        return self._web3
+        for event in events:
+            callback(event)
+            # time.sleep(0.1)
 
-    @property
-    def network_name(self):
-        """Give the network name."""
-        network_id = self._web3.version.network
-        switcher = {
-            1: 'Main',
-            2: 'orden',
-            3: 'Ropsten',
-            4: 'Rinkeby',
-            42: 'Kovan',
-        }
-        return switcher.get(network_id, 'development')
+        # always take a rest
+        time.sleep(0.1)
 
-    # static methods
-    @staticmethod
-    def watcher(event_filter, callback):
-        while True:
-            try:
-                events = event_filter.get_all_entries()
-            except ValueError as err:
-                # ignore error, but log it
-                print('Got error grabbing keeper events: ', str(err))
-                events = []
 
-            for event in events:
-                callback(event)
-                # time.sleep(0.1)
+def convert_to_bytes(web3, data):
+    return web3.toBytes(text=data)
 
-            # always take a rest
-            time.sleep(0.1)
 
-    def convert_to_bytes(self, data):
-        return self._web3.toBytes(text=data)
+def convert_to_string(web3, data):
+    return web3.toHex(data)
 
-    def convert_to_string(self, data):
-        return self._web3.toHex(data)
 
-    def convert_to_text(self, data):
-        return self._web3.toText(data)
+def convert_to_text(web3, data):
+    return web3.toText(data)
--- tests/test_ocean.py
+++ tests/test_ocean.py
@@ -11,7 +11,7 @@ from web3 import Web3
 
 from squid_py.config import Config
 from squid_py.ocean import Ocean
-from squid_py.utils.utilities import Utilities
+from squid_py.utils import utilities
 
 
 def test_ocean_instance():
@@ -95,19 +95,17 @@ def _test_ocean_contracts_with_conf(caplog):
 
 
 def test_split_signature():
-    utilities = Utilities(Web3)
     signature = b'\x19\x15!\xecwnX1o/\xdeho\x9a9\xdd9^\xbb\x8c2z\x88!\x95\xdc=\xe6\xafc\x0f\xe9\x14\x12\xc6\xde\x0b\n\xa6\x11\xc0\x1cvv\x9f\x99O8\x15\xf6f\xe7\xab\xea\x982Ds\x0bX\xd9\x94\xa42\x01'
-    split_signature = utilities.split_signature(signature=signature)
+    split_signature = utilities.split_signature(Web3, signature=signature)
     assert split_signature.v == 28
     assert split_signature.r == b'\x19\x15!\xecwnX1o/\xdeho\x9a9\xdd9^\xbb\x8c2z\x88!\x95\xdc=\xe6\xafc\x0f\xe9'
     assert split_signature.s == b'\x14\x12\xc6\xde\x0b\n\xa6\x11\xc0\x1cvv\x9f\x99O8\x15\xf6f\xe7\xab\xea\x982Ds\x0bX\xd9\x94\xa42'
 
 
 def test_convert():
-    utilities = Utilities(Web3)
     input_text = "my text"
-    print("output %s" % utilities.convert_to_string(utilities.convert_to_bytes(input_text)))
-    assert utilities.convert_to_text(utilities.convert_to_bytes(input_text)) == input_text
+    print("output %s" % utilities.convert_to_string(Web3, utilities.convert_to_bytes(Web3, input_text)))
+    assert utilities.convert_to_text(Web3, utilities.convert_to_bytes(Web3, input_text)) == input_text
 
 
 def _test_legacy_accounts_legacy():
