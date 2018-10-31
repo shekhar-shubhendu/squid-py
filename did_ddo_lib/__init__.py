3f3f2ee44ce06974f79d88288d31d4449a271ad2
Bill Barman <bill@anantya.com>
2018-10-31 12:18:43 +0800

    fist test pass

--- did_ddo_lib/__init__.py
+++ did_ddo_lib/__init__.py
@@ -20,14 +20,3 @@ from did_ddo_lib.main import (
     did_generate_from_id,
     get_id_from_did,
 )
-
-from did_ddo_lib.ocean_ddo import (
-    OceanDDO,
-)
-
-from did_ddo_lib.public_key_base import (
-    PUBLIC_KEY_STORE_TYPE_PEM,
-    PUBLIC_KEY_STORE_TYPE_HEX,
-    PUBLIC_KEY_STORE_TYPE_BASE64,
-    PUBLIC_KEY_STORE_TYPE_BASE85,
-)
--- tests/test_ddo.py
+++ tests/test_ddo.py
@@ -7,21 +7,19 @@ import pytest
 import secrets
 from web3 import Web3
 
-from did_ddo_lib import (
+from did_ddo_lib.main import (
     did_generate,
     did_generate_from_ddo,
-    did_parse,
     did_validate,
-    is_did_valid,
-    did_generate_from_id,
-    get_id_from_did,
-    OceanDDO,
+)
+
+from squid_py.ddo import (
+    DDO,
     PUBLIC_KEY_STORE_TYPE_PEM,
     PUBLIC_KEY_STORE_TYPE_HEX,
     PUBLIC_KEY_STORE_TYPE_BASE64,
     PUBLIC_KEY_STORE_TYPE_BASE85,
 )
-from squid_py.ddo import DDO
 
 public_key_store_types = [
     PUBLIC_KEY_STORE_TYPE_PEM,
@@ -34,108 +32,11 @@ TEST_SERVICE_TYPE = 'ocean-meta-storage'
 TEST_SERVICE_URL = 'http://localhost:8005'
 
 
-def test_did():
-    test_id = secrets.token_hex(32)
-    test_path = 'test_path'
-    test_fragment = 'test_fragment'
-    test_method = 'abcdefghijklmnopqrstuvwxyz0123456789'
-    all_id = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.'
-    valid_did = 'did:op:{0}'.format(test_id)
-
-    assert did_generate(test_id) == valid_did
-    assert did_parse(valid_did)['id'] == test_id
-
-    # all valid method values are supported
-    valid_method_did = 'did:{0}:{1}'.format(test_method, test_id)
-    assert did_generate(test_id, method=test_method) == valid_method_did
-
-    # split method
-    assert did_parse(valid_method_did)['method'] == test_method
-
-    # invalid method chars are removed
-    assert did_generate(test_id, method=test_method + '!@#$%^&') == valid_method_did
-
-    # all valid method and id's are accepted
-    valid_id_method_did = 'did:{0}:{1}'.format(test_method, all_id)
-    assert did_generate(all_id, method=test_method) == valid_id_method_did
-
-    # split id and method
-    assert did_parse(valid_id_method_did)['method'] == test_method
-    assert did_parse(valid_id_method_did)['id'] == all_id
-
-    # invalid id values are masked out
-    assert did_generate(all_id + '%^&*()_+=', method=test_method) == valid_id_method_did
-
-    # path can be appended
-    valid_path_did = 'did:op:{0}/{1}'.format(test_id, test_path)
-    assert did_generate(test_id, test_path) == valid_path_did
-
-    assert did_parse(valid_path_did)['path'] == test_path
-
-    # append path and fragment
-    valid_path_fragment_did = 'did:op:{0}/{1}#{2}'.format(test_id, test_path, test_fragment)
-    assert did_generate(test_id, test_path, test_fragment) == valid_path_fragment_did
-
-    # assert split of path and fragment
-    assert did_parse(valid_path_fragment_did)['path'] == test_path
-    assert did_parse(valid_path_fragment_did)['fragment'] == test_fragment
-
-    # append fragment
-    valid_fragment_did = 'did:op:{0}#{1}'.format(test_id, test_fragment)
-    assert did_generate(test_id, fragment=test_fragment) == valid_fragment_did
-
-    # assert split offragment
-    assert did_parse(valid_fragment_did)['fragment'] == test_fragment
-
-    with pytest.raises(TypeError):
-        did_parse(None)
-
-    # test invalid in bytes
-    with pytest.raises(TypeError):
-        assert did_parse(valid_did.encode())
-
-
-    # test is_did_valid
-    assert is_did_valid(valid_did)
-    assert not is_did_valid('did:op:{}'.format(all_id))
-    assert not is_did_valid('did:eth:{}'.format(test_id))
-    assert not is_did_valid('op:{}'.format(test_id))
-
-    with pytest.raises(TypeError):
-        is_did_valid(None)
-
-
-    # test invalid in bytes
-    with pytest.raises(TypeError):
-        assert is_did_valid(valid_did.encode())
-
-
-    valid_did_text = 'did:op:{}'.format(test_id)
-    assert did_generate_from_id(test_id) == valid_did_text
-
-    # accept hex string from Web3 py
-    assert did_generate_from_id(Web3.toHex(hexstr=test_id)) == valid_did_text
-
-    #accepts binary value
-    assert did_generate_from_id(Web3.toBytes(hexstr=test_id)) == valid_did_text
-
-    with pytest.raises(TypeError):
-        did_generate_from_id(None)
-
-    with pytest.raises(TypeError):
-        did_generate_from_id({'bad': 'value'})
-
-    assert did_generate_from_id('') == 'did:op:0'
-    assert get_id_from_did(valid_did_text) == test_id
-    assert get_id_from_did('did:op1:011') == None
-    assert get_id_from_did('did:op:0') == '0'
-
-
 def test_creating_ddo():
     did_id = secrets.token_hex(32)
     did = did_generate(did_id)
     assert did
-    ddo = OceanDDO(did)
+    ddo = DDO(did)
     assert ddo
     private_keys = []
     for public_key_store_type in public_key_store_types:
@@ -165,13 +66,13 @@ def test_creating_ddo():
         assert ddo.validate_proof()
         ddo_text_proof_hash = ddo.calculate_hash()
 
-    ddo = OceanDDO(ddo_text=ddo_text_proof)
+    ddo = DDO(json_text=ddo_text_proof)
     assert ddo.validate()
     assert ddo.is_proof_defined()
     assert ddo.validate_proof()
     assert ddo.calculate_hash() == ddo_text_proof_hash
 
-    ddo = OceanDDO(ddo_text=ddo_text_no_proof)
+    ddo = DDO(json_text=ddo_text_no_proof)
     assert ddo.validate()
     # valid proof should be false since no static proof provided
     assert not ddo.is_proof_defined()
@@ -183,7 +84,7 @@ def test_creating_ddo_embedded_public_key():
     test_id = secrets.token_hex(32)
     did = did_generate(test_id)
     assert did
-    ddo = OceanDDO(did)
+    ddo = DDO(did)
     assert ddo
     private_keys = []
     for public_key_store_type in public_key_store_types:
@@ -204,7 +105,7 @@ def test_creating_ddo_embedded_public_key():
 def test_creating_did_using_ddo():
     # create an empty ddo
     test_id = secrets.token_hex(32)
-    ddo = OceanDDO()
+    ddo = DDO()
     assert ddo
     private_keys = []
     for public_key_store_type in public_key_store_types:
@@ -240,7 +141,7 @@ def test_load_ddo_json():
 
     SAMPLE_DDO_JSON_STRING = json.dumps(SAMPLE_DDO_JSON_DICT)
 
-    this_ddo = OceanDDO(ddo_text = SAMPLE_DDO_JSON_STRING)
+    this_ddo = DDO(json_text = SAMPLE_DDO_JSON_STRING)
     service = this_ddo.get_service('Metadata')
     assert service
     assert service.get_type() == 'Metadata'
@@ -252,7 +153,7 @@ def test_ddo_dict():
     sample_ddo_path = pathlib.Path.cwd() / 'tests/resources/ddo' / 'ddo_sample1.json'
     assert sample_ddo_path.exists(), "{} does not exist!".format(sample_ddo_path)
 
-    ddo1 = DDO.from_json_file(sample_ddo_path)
+    ddo1 = DDO(json_filename=sample_ddo_path)
     assert ddo1.is_valid
-    assert len(ddo1.keys()) == 5
-    assert ddo1['id'] == 'did:op:3597a39818d598e5d60b83eabe29e337d37d9ed5af218b4af5e94df9f7d9783a'
+    assert len(ddo1.public_keys) == 3
+    assert ddo1.did == 'did:op:3597a39818d598e5d60b83eabe29e337d37d9ed5af218b4af5e94df9f7d9783a'
