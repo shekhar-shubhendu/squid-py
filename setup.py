ee97cbb5117f0ccb413f29d26e954b02311202e5
MarcusJones <jones.0bj3@gmail.com>
2018-11-06 16:06:03 +0100

    fix Include ddo module!

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     include_package_data=True,
     keywords='squid-py',
     name='squid-py',
-    packages=find_packages(include=['squid_py','squid_py.keeper','squid_py.utils','did_ddo_lib']),
+    packages=find_packages(include=['squid_py','squid_py.keeper','squid_py.utils','squid_py.ddo']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
