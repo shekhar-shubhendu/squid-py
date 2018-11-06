89c1b60f30b23183b6202283932730ff46c53a84
MarcusJones <jones.0bj3@gmail.com>
2018-11-06 16:12:24 +0100

    Add dependencies

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.md') as history_file:
     history = history_file.read()
 
-requirements = ['keeper-contracts']
+requirements = ['keeper-contracts', 'web3==4.7.1', 'pyyaml', 'coloredlogs']
 
 setup_requirements = ['pytest-runner', ]
 
