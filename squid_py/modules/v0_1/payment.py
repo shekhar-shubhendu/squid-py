29204632eeb1922a6e67a5c666572520675637a7
Lev Berman <ldmberman@gmail.com>
2018-11-12 16:25:59 +0100

    Implement releasePayment event callback

--- squid_py/modules/v0_1/payment.py
+++ squid_py/modules/v0_1/payment.py
@@ -31,3 +31,30 @@ def lockPayment(web3, contract_path, account, service_agreement_id, service_defi
         parameters['price'],
         transact={'from': account},
     )
+
+
+def releasePayment(web3, contract_path, account, service_agreement_id, service_definition,
+                   *args, **kwargs):
+    """ Checks if the releasePayment condition has been fulfilled and if not calls
+        PaymentConditions.releasePayment smart contract function.
+    """
+    payment_conditions, abi, payment_condition_definition = get_condition_contract_data(
+        web3,
+        contract_path,
+        service_definition,
+        'releasePayment',
+    )
+
+    service_agreement_address = service_definition['serviceAgreementContract']['address']
+    if is_condition_fulfilled(web3, contract_path, service_definition['templateId'],
+                              service_agreement_id, service_agreement_address,
+                              payment_conditions.address, abi, 'releasePayment'):
+        return
+
+    parameters = payment_condition_definition['parameters']
+    payment_conditions.releasePayment(
+        service_agreement_id.encode(),
+        parameters['did'].encode(),
+        parameters['price'],
+        transact={'from': account},
+    )
