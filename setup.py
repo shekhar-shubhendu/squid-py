e97ca013742e2ae5207a03e53f0f7533f4d06b64
MarcusJones <jones.0bj3@gmail.com>
2018-11-28 10:31:51 +0100

    Bump version: 0.2.10 → 0.2.11

--- setup.py
+++ setup.py
@@ -88,6 +88,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/squid-py',
-    version='0.2.10',
+    version='0.2.11',
     zip_safe=False,
 )
